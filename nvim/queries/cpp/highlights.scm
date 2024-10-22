5b8c5f151388758b9b92bd7d73b991fcda182fe8
Colton Hill <ctj.cchill@gmail.com>
2024-10-21 20:57:12 -0600

    update node install instructions

---
+++ nvim/queries/cpp/highlights.scm
@@ -0,0 +1,9 @@
+;; extends
+
+;; Match variables like: int badname = 0;
+(init_declarator
+  declarator: (identifier) @spell)
+
+;; Match parameters like: void func(std::string badname)
+(parameter_declaration
+  declarator: (identifier) @spell)
